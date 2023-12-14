bfd963fd4e6c43201158d587fe887e5532d689e9
liubo00 <liubo@zerozero.cn>
2023-12-14 20:00:16 +0800

    rgftyjhjhjg

--- example/example01/complile.sh
+++ example/example01/complile.sh
@@ -3,9 +3,12 @@
 # complile.sh
 
 # 源文件路径 ./proto
-# 目标文件路径 ./build/proto
+# 目标文件路径 ./build/proto、
 
-
+# 注意路径
+mkdir -p proto
 #protoc  --proto_path=./proto  --cpp_out=./build/proto   ./proto/*.proto
-protoc  --proto_path=./proto  --cpp_out=./build/proto   people_info.proto
+
+# 注意路径
+protoc  --proto_path=../proto  --cpp_out=../build/proto   ../proto/people_info.proto
 
