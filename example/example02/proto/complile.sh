356381c382b5a1718e99a4a200ea3fda4d56602b
liubo00 <liubo@zerozero.cn>
2023-12-18 17:21:58 +0800

    dfvdfbh

---
+++ example/example02/proto/complile.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# complile.sh
+
+# 源文件路径 ./proto
+# 目标文件路径 ./build/proto、
+
+# 注意路径
+# mkdir -p proto
+# protoc  --proto_path=./proto  --cpp_out=./build/proto   ./proto/*.proto
+
+# 注意路径
+protoc  --proto_path=../proto  --cpp_out=../proto   ../proto/addressbook.proto
+
