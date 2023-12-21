5c5ef97f11c49010bd9f1c2462f9846d101676d7
liubo00 <liubo@zerozero.cn>
2023-12-21 17:25:28 +0800

    ghgjghj

--- example/example02/proto/complile.sh
+++ example/example02/proto/complile.sh
@@ -10,5 +10,7 @@
 # protoc  --proto_path=./proto  --cpp_out=./build/proto   ./proto/*.proto
 
 # 注意路径
-protoc  --proto_path=../proto  --cpp_out=../proto   ../proto/addressbook.proto
+echo "---proto---"
+
+protoc --experimental_allow_proto3_optional --proto_path=../proto  --cpp_out=../proto   ../proto/addressbook.proto
 
