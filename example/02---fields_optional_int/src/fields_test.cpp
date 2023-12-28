64d661bdca8cbd8e326f6fa64e14445a8316c786
liubo00 <liubo@zerozero.cn>
2023-12-28 10:28:37 +0800

    tyjuiyui

--- example/02---fields_optional_int/src/fields_test.cpp
+++ example/02---fields_optional_int/src/fields_test.cpp
@@ -12,43 +12,40 @@ int main()
     // compatible with the version of the headers we compiled against.
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-    basic::MyNumber number;
+    mynumber::MyNumber testnumber;
 
     // 获取 opt_number()
     cout << "----------------默认值------------------" << endl;
-    cout << "opt_number: " << number.opt_number() << endl;
-    cout << "imp_number: " << number.imp_number() << endl;
+    cout << "opt_number: " << testnumber.opt_number() << endl;
+    cout << "imp_number: " << testnumber.imp_number() << endl;
 
     // 储存 set_opt_number()
-    number.set_opt_number(20);
-    number.set_imp_number(40);
+    testnumber.set_opt_number(20);
+    testnumber.set_imp_number(40);
 
     cout << "----------------储存值------------------" << endl;
-    cout << "opt_number: " << number.opt_number() << endl;
-    cout << "imp_number: " << number.imp_number() << endl;
+    cout << "opt_number: " << testnumber.opt_number() << endl;
+    cout << "imp_number: " << testnumber.imp_number() << endl;
 
     cout << "----------------判断值------------------" << endl;
-    cout << "has_opt_number: " << number.has_opt_number() << endl;
-    //cout << "has_opt_number: " << number.has_imp_number() << endl; //has_imp_number()没有此函数
+    cout << "has_opt_number: " << testnumber.has_opt_number() << endl;
+    //cout << "has_opt_number: " << testnumber.has_imp_number() << endl; //has_imp_number()没有此函数
 
     // 清除 set_opt_number()
-    number.clear_opt_number();
-    number.clear_imp_number();
+    testnumber.clear_opt_number();
+    testnumber.clear_imp_number();
 
     cout << "----------------清除值------------------" << endl;
-    cout << "opt_number: " << number.opt_number() << endl;
-    cout << "imp_number: " << number.imp_number() << endl;
+    cout << "opt_number: " << testnumber.opt_number() << endl;
+    cout << "imp_number: " << testnumber.imp_number() << endl;
 
     cout << "----------------判断值------------------" << endl;
-    cout << "has_opt_number: " << number.has_opt_number() << endl;
-
+    cout << "has_opt_number: " << testnumber.has_opt_number() << endl;
 
     // 储存 set_opt_number()
-    number.set_opt_number(0);
+    testnumber.set_opt_number(0);
     cout << "----------------设0判断值------------------" << endl;
-    cout << "has_opt_number: " << number.has_opt_number() << endl; //结果为一
-
-
+    cout << "has_opt_number: " << testnumber.has_opt_number() << endl; //结果为一
 
     return 0;
 }
--- example/03---fields_optional_string/src/fields_test.cpp
+++ example/03---fields_optional_string/src/fields_test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "../build/proto/basic.pb.h"
+#include "../build/proto/mystring.pb.h"
 
 using std::cout;
 using std::endl;
@@ -12,41 +12,41 @@ int main()
     // compatible with the version of the headers we compiled against.
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-    basic::MyNumber number;
+    mystring::MyString teststring;
 
     // 获取 opt_number()
     cout << "----------------默认值------------------" << endl;
-    cout << "opt_number: " << number.opt_number() << endl;
-    cout << "imp_number: " << number.imp_number() << endl;
+    cout << "opt_string: " << teststring.opt_string() << endl;
+    // cout << "imp_number: " << number.imp_number() << endl;
 
-    // 储存 set_opt_number()
-    number.set_opt_number(20);
-    number.set_imp_number(40);
+    // // 储存 set_opt_number()
+    // number.set_opt_number(20);
+    // number.set_imp_number(40);
 
-    cout << "----------------储存值------------------" << endl;
-    cout << "opt_number: " << number.opt_number() << endl;
-    cout << "imp_number: " << number.imp_number() << endl;
+    // cout << "----------------储存值------------------" << endl;
+    // cout << "opt_number: " << number.opt_number() << endl;
+    // cout << "imp_number: " << number.imp_number() << endl;
 
-    cout << "----------------判断值------------------" << endl;
-    cout << "has_opt_number: " << number.has_opt_number() << endl;
-    //cout << "has_opt_number: " << number.has_imp_number() << endl; //has_imp_number()没有此函数
+    // cout << "----------------判断值------------------" << endl;
+    // cout << "has_opt_number: " << number.has_opt_number() << endl;
+    // //cout << "has_opt_number: " << number.has_imp_number() << endl; //has_imp_number()没有此函数
 
-    // 清除 set_opt_number()
-    number.clear_opt_number();
-    number.clear_imp_number();
+    // // 清除 set_opt_number()
+    // number.clear_opt_number();
+    // number.clear_imp_number();
 
-    cout << "----------------清除值------------------" << endl;
-    cout << "opt_number: " << number.opt_number() << endl;
-    cout << "imp_number: " << number.imp_number() << endl;
+    // cout << "----------------清除值------------------" << endl;
+    // cout << "opt_number: " << number.opt_number() << endl;
+    // cout << "imp_number: " << number.imp_number() << endl;
 
-    cout << "----------------判断值------------------" << endl;
-    cout << "has_opt_number: " << number.has_opt_number() << endl;
+    // cout << "----------------判断值------------------" << endl;
+    // cout << "has_opt_number: " << number.has_opt_number() << endl;
 
 
-    // 储存 set_opt_number()
-    number.set_opt_number(0);
-    cout << "----------------设0判断值------------------" << endl;
-    cout << "has_opt_number: " << number.has_opt_number() << endl; //结果为一
+    // // 储存 set_opt_number()
+    // number.set_opt_number(0);
+    // cout << "----------------设0判断值------------------" << endl;
+    // cout << "has_opt_number: " << number.has_opt_number() << endl; //结果为一
 
 
 
