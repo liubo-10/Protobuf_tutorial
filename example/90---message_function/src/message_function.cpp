bff30734e1d63d768a9209d61ae6295756425c2f
liubo00 <liubo@zerozero.cn>
2024-01-11 14:35:06 +0800

    gfhfdghfj

--- example/90---message_function/src/message_function.cpp
+++ example/90---message_function/src/message_function.cpp
@@ -6,12 +6,6 @@
 
 
 
-// 
-// void MergeFrom(const Student& from);
-// void Clear();
-// bool IsInitialized() const;
-
-
 
 //！
 //！ ********************************************************************
@@ -36,14 +30,15 @@ int main() {
     //！ ********************************************************************
     //！ 测试函数 void CopyFrom(const Student& from);
     //！ ********************************************************************
+    cout << "\n----------------CopyFrom 测试------------------" << endl;
     Student1.set_id(1);
     Student1.set_name("Tom");
     Student1.set_email("qq@123");
-    Student1.add_phone()->set_number("12345");
-    Student1.mutable_phone(0)->set_type(myprotobuf::Student::MOBILE);
+    Student1.add_phone();// 先要添加才能使用
+    Student1.mutable_phone(0)->set_number("12345");
+    Student1.mutable_phone(0)->set_type(myprotobuf::Student::HOME);
 
     Student2.CopyFrom(Student1);
-
 
     cout << "Student2.id: " << Student2.id() << endl;
     cout << "Student2.name: " << Student2.name() << endl;
@@ -51,57 +46,44 @@ int main() {
     cout << "Student2 number: " << Student2.mutable_phone(0)->number() << endl;
     cout << "Student2 type: " << Student2.mutable_phone(0)->type() << endl;
 
+    //！ ********************************************************************
+    //！ 测试函数 void MergeFrom(const Student& from);
+    //！ 与 CopyFrom结果一样
+    //！ ********************************************************************
+    cout << "\n----------------MergeFrom 测试------------------" << endl;
+    myprotobuf::Student Student3;
+    Student3.set_id(3);
 
+    Student3.MergeFrom(Student1);
 
+    cout << "Student3.id: " << Student3.id() << endl;
+    cout << "Student3.name: " << Student3.name() << endl;
+    cout << "Student3.email: " << Student3.email() << endl;
+    cout << "Student3 number: " << Student3.mutable_phone(0)->number() << endl;
+    cout << "Student3 type: " << Student3.mutable_phone(0)->type() << endl;
 
-
-
-    // cout << "\n----------------默认值测试------------------" << endl;
-    // cout << "optnumber: " << testnumber.optnumber() << endl; // 0
 
     //！ ********************************************************************
-    //！ 存储值测试
-    //！ 测试函数 set_optnumber()
+    //！ 测试函数 void Clear();
     //！ ********************************************************************
-    // cout << "\n----------------存储值测试------------------" << endl;
-    // testnumber.set_optnumber(11);
-    // cout << "optnumber: " << testnumber.optnumber() << endl;
+    cout << "\n----------------Clear 测试------------------" << endl;
 
-    // testnumber.set_optnumber(22);
-    // cout << "optnumber: " << testnumber.optnumber() << endl;
+    Student3.Clear();
 
-    //！ ********************************************************************
-    //！ 清除值测试
-    //！ 测试函数 clear_optnumber()
-    //！ optnumber() has_optnumber()结果都为0
-    //！ ********************************************************************
-    // cout << "\n----------------清除值测试------------------" << endl;
-    // cout <<   "----------------清除之前--------------------" << endl;
-    // cout << "optnumber: " << testnumber.optnumber() << endl;
-    // cout << "has_optnumber: " << testnumber.has_optnumber() << endl;
+    cout << "Student3.id: " << Student3.id() << endl;
+    cout << "Student3.name: " << Student3.name() << endl;
+    cout << "Student3.email: " << Student3.email() << endl;
+    //cout << "Student3 number: " << Student3.mutable_phone(0)->number() << endl;//error
+    //cout << "Student3 type: " << Student3.mutable_phone(0)->type() << endl;//error
 
-    // testnumber.clear_optnumber();
-    
-    // cout << "----------------清除之后--------------------" << endl;
-    // cout << "optnumber: " << testnumber.optnumber() << endl;
-    // cout << "has_optnumber: " << testnumber.has_optnumber() << endl;
 
     //！ ********************************************************************
-    //！ 有无赋值测试
-    //！ 测试函数 has_optnumber()
-    //！ has_impnumber()隐式类型没有此函数
-    //！ set_optnumber(0),赋值与默认值相同，has_optnumber()结果为1
+    //！ 测试函数 void IsInitialized();
+    //！ bool Student::IsInitialized() const {
+    //！   return true;
+    //！ }
     //！ ********************************************************************
-    // cout << "\n----------------有无赋值测试------------------" << endl;
-    // testnumber.clear_optnumber();
-    // cout << "----------------清除之后--------------------" << endl;
-    // cout << "has_optnumber: " << testnumber.has_optnumber() << endl;
 
-    // testnumber.set_optnumber(0);
-    // cout << "----------------设0后判断有无赋值------------------" << endl;
-    // cout << "has_optnumber: " << testnumber.has_optnumber() << endl;
-
-    // cout << "\n----------------end------------------" << endl;
     return 0;
 }
 
